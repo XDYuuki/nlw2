06efa2124e449bdc87cfa5c1f617ac72da4b50af
Gabriel Rocha <gabriel.lbr@outlook.com>
2020-08-04 00:21:13 -0300

    React structure and global.css

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//styles Global
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button>Hey! Click here...</button>
+    </div>
+  );
+}
+
+export default App;
